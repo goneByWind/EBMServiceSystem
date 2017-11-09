f6072edb7fb0e5e57585e8d52152adf7205b51a4
zar <a18502436094@163.com>
2017-11-09 16:00:02 +0800

    用户名,密码验证完成

--- src/main/java/com/lanou/admin/controller/LoginUser.java
+++ src/main/java/com/lanou/admin/controller/LoginUser.java
@@ -1,10 +1,15 @@
 package com.lanou.admin.controller;
 
 import com.lanou.exception.CustomerException;
+import com.lanou.utils.AjaxResult;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,16 +24,20 @@ public class LoginUser {
     }
 
     // 用户登录表单验证
+    @ResponseBody
     @RequestMapping(value = "/loginsubmit")
-    public String loginsubmit(HttpServletRequest request) throws Exception {
+    public AjaxResult loginsubmit(HttpServletRequest request,
+                                  @RequestParam("username") String username,
+                                  @RequestParam("password") String password) throws Exception {
         // shiro在认证过程中出现错误后,将异常类路径通过request返回
-        String exceptionClassName = (String) request.getAttribute("shiroLoginFailure");
-        if (exceptionClassName.equals(UnknownAccountException.class.getName())) {
-            throw new CustomerException("账户名不存在");
-        } else if (IncorrectCredentialsException.class.getName().equals(exceptionClassName)) {
-            throw new CustomerException("密码错误");
-        } else {
-            throw new Exception();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        try {
+            SecurityUtils.getSubject().login(token);
+            return new AjaxResult(0, "登录成功");
+        } catch (UnknownAccountException uae) {
+            return new AjaxResult(1, "用户名错误");
+        } catch (IncorrectCredentialsException ice) {
+            return new AjaxResult(1, "密码错误");
         }
     }
 }
--- src/main/java/com/lanou/admin/mapper/SysUserMapper.java
+++ src/main/java/com/lanou/admin/mapper/SysUserMapper.java
@@ -1,6 +1,7 @@
 package com.lanou.admin.mapper;
 
 import com.lanou.admin.bean.SysUser;
+import org.apache.ibatis.annotations.Param;
 
 public interface SysUserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +15,12 @@ public interface SysUserMapper {
     int updateByPrimaryKeySelective(SysUser record);
 
     int updateByPrimaryKey(SysUser record);
+
+    /**
+     * 通过用户名查找用户
+     *
+     * @param username 用户名
+     * @return 用户
+     */
+    SysUser selectByUserName(@Param("username") String username);
 }
