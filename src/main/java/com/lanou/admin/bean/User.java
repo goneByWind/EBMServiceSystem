bad793e2fa0c871263c8ee7c146a6e4f2949e0d1
dllo <15140211872@163.com>
2017-11-09 11:00:45 +0800

    user表单生成

--- src/main/java/com/lanou/admin/bean/User.java
+++
@@ -1,7 +0,0 @@
-package com.lanou.admin.bean;
-
-/**
- * @author zar on 2017/11/9.
- */
-public class User {
-}
---
+++ src/main/java/com/lanou/admin/mapper/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.lanou.admin.mapper;
+
+import com.lanou.admin.bean.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}
